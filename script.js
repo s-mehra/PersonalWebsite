adc7c3e199a5d87fd9f1f12c41caad727f5f2cb3
Soham Mehra <mehrasoham.mail@gmail.com>
2023-01-18 20:42:08 -0500

    refactored style and script file

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-let toggle = document.querySelector('#dark-mode-toggle');
-let header = document.querySelector('header');
-let btn = document.querySelector('.btn');
-
-toggle.addEventListener('click', () => {
-    if (toggle.checked) {
-        header.style.setProperty('background', '#253A52');
-        document.body.style.setProperty('background','#253A52');
-        header.style.setProperty('color', '#FFFFE3');
-        document.body.style.setProperty('color','#FFFFE3');
-        btn.style.setProperty('background', '#A9FE75');
-        btn.style.setProperty('color', '#253A52');
-        document.querySelector('.project-work').style.setProperty('color', '#A9FE75');
-        document.querySelector('.toggle-switch-dot').style.setProperty('background','#FFFFE3');
-        document.querySelector('.toggle-switch-border').style.borderColor =  '#FFFFE3';
-
-    } else {
-        header.style.setProperty('background', '#FFFFE3');
-        document.body.style.setProperty('background','#FFFFE3');
-        header.style.setProperty('color', '#253A52');
-        document.body.style.setProperty('color','#253A52');
-        btn.style.setProperty('background', '#1348DA');
-        btn.style.setProperty('color', '#FFFFE3');
-        document.querySelector('.project-work').style.setProperty('color', '#1348DA');
-        document.querySelector('.toggle-switch-dot').style.setProperty('background','#253A52');
-        document.querySelector('.toggle-switch-border').style.borderColor =  '#253A52';
+class CssPropControl {
+    constructor(element) {
+      this.element = element
     }
-    
-});
+    get(varName) {
+      return getComputedStyle(this.element).getPropertyValue(varName)
+    }
+    set(varName, val) {
+      return this.element.style.setProperty(varName, val)
+    }
+  }
+  
+  const bodyCssProps = new CssPropControl(document.body)
+  
+  let toggle = document.querySelector('#dark-mode-toggle')
+  toggle.addEventListener('click', () => { 
+    let mode = toggle.checked ? 'dark' : 'light'
+    bodyCssProps.set('--background', bodyCssProps.get(`--${mode}-background`))
+    bodyCssProps.set('--primary', bodyCssProps.get(`--${mode}-primary`))
+    bodyCssProps.set('--link', bodyCssProps.get(`--${mode}-link`))
+  })
+  
